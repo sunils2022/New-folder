dc0d4bd6a4dc4ef3b5992a5aa783392b68687f27
sunil <sunilsolunke218@gmail.com>
2022-12-26 10:52:33 +0530

    Arraylist add

---
+++ myproject/src/com/myproject/Demo1.java
@@ -0,0 +1,13 @@
+package com.myproject;
+
+import java.util.ArrayList;
+
+public class Demo1{
+public static void main(String[] args) {
+	ArrayList al =new ArrayList();
+	al.add(10);
+	al.add(20);
+	System.out.println(al);
+
+}
+}
