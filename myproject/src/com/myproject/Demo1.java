afaf34de1093fe767a27221f66eb10b4f1bb5a88
sunil <sunilsolunke218@gmail.com>
2022-12-26 11:51:51 +0530

    array 30 add

--- myproject/src/com/myproject/Demo1.java
+++ myproject/src/com/myproject/Demo1.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Demo1{
 public static void main(String[] args) {
 	ArrayList al =new ArrayList();
-	al.add(10);
-	al.add(20);
+	
+	al.add(30);
 	System.out.println(al);
 
 }
